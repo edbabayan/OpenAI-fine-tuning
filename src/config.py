71c08dc4b90cf81e90b1b0d14b422d9de4d2f1ad
eduard <eduard.babayan@cognaize.com>
2025-01-03 14:07:55 +0400

    ADD: data preprocessing

--- src/config.py
+++ src/config.py
@@ -4,3 +4,9 @@ from pathlib import Path
 class CFG:
     root = Path(__file__).parent.parent.absolute()
     data_path = root.joinpath("data")
+    model = "gpt-4o-mini"
+
+    system_prompt = (
+        "Given the medical description report, classify it into one of these categories: "
+        "[Cardiovascular / Pulmonary, Gastroenterology, Neurology, Radiology, Surgery]"
+    )
